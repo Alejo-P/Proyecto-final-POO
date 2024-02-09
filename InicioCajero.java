c8998d25077ff77b84dcdf74c542897ad87e6b3c
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-09 11:56:40 -0500

    Commit inicial 5

--- InicioCajero.java
+++ InicioCajero.java
@@ -2,4 +2,8 @@ import javax.swing.*;
 
 public class InicioCajero {
     JPanel Inicio_Cajero;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JPasswordField passwordField1;
+    private JButton loginButton;
 }
