0734f545183c7a13c49e8b8786b976f22f43c2be
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-09 11:38:08 -0500

    Commit inicial 3

---
+++ InicioCajero.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class InicioCajero {
+    JPanel Inicio_Cajero;
+}
--- main.java
+++ main.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Inicio Cajero");
+        frame.setContentPane(new InicioCajero().Inicio_Cajero);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
