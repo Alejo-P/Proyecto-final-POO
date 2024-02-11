1f6f5cab38ffe93d325d264a203631dab6fc5741
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-11 06:57:17 -0500

    Pantalla Silvia

---
+++ PantallaCajero.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class PantallaCajero {
+    private JPanel PantCaj;
+    private JComboBox comboBox1;
+}
---
+++ PantallaTransaccion.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class PantallaTransaccion {
+    private JPanel PantallaTransac;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JButton confirmarButton;
+    private JButton imprimirFacturaButton;
+    private JButton volverAlMenúButton;
+    private JButton borrarButton1;
+}
