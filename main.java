b1eb58e26f810fe048d9664c2148107417ebbd0e
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-16 10:03:35 -0500

    Cambios Silvia

--- main.java
+++ main.java
@@ -1,13 +1,44 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class main {
+    private static Point initialClick;
     static JFrame frame = new JFrame("Inicio Cajero");
+
     public static void main(String[] args) {
         frame.setContentPane(new LOGIN().Inicio_Cajero);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(true); //Elimina la barra de titulo
+
+
+        //Permite mover la ventana arrastrandola con el mouse----------------------
+        frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                initialClick=e.getPoint();
+                frame.getComponentAt(initialClick);
+            }
+        });
+        frame.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int thisX=frame.getLocation().x;
+                int thisY= frame.getLocation().y;
+
+                int xMoved=thisX+e.getX()-initialClick.x;
+                int yMoved=thisY+e.getY()-initialClick.y;
+
+                frame.setLocation(xMoved, yMoved);
+            }
+        });
+        //------------------------------------------------------
+
         //hace que la pantalla aparesca en el centro
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
+
     }
 }
