c0ac5b3c79203b0d5fea5e97a732e7896f408450
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-15 19:35:17 -0500

    Cambios Silvia

--- InicioCajero.java
+++ InicioCajero.java
@@ -1,9 +1,40 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InicioCajero {
     JPanel Inicio_Cajero;
     private JTextField textField1;
     private JComboBox comboBox1;
     private JPasswordField passwordField1;
-    private JButton loginButton;
+    private JButton ingresarButton;
+    private JButton borrarButton;
+
+    public InicioCajero() {
+        JButton botonBorrar = new JButton("Borrar");
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                borrarCampos();
+            }
+        });
+    }
+
+    private void borrarCampos(){
+        String usuarioTexto=textField1.getText();
+        String contraseñaTexto=new String(passwordField1.getPassword());
+        // Verifica si hay caracteres en el campo de usuario antes de intentar borrar
+        if(!usuarioTexto.isEmpty()){
+            // Borra la última letra del campo de usuario
+            textField1.setText(usuarioTexto.substring(0, usuarioTexto.length()-1));
+        }
+        if(!contraseñaTexto.isEmpty()){
+            passwordField1.setText(contraseñaTexto.substring(0, contraseñaTexto.length()-1));
+
+
+        }
+
+
+
+    }
 }
