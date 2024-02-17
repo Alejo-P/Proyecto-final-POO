ca103664f1a8bac5204c27f070b38a281ba374ee
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-16 22:23:44 -0500

    Arreglo de un commit Obtener el foco de un campo para borrar el ultimo digito

--- LOGIN.java
+++ LOGIN.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class LOGIN {
     JPanel Inicio_Cajero;
@@ -11,13 +13,65 @@ public class LOGIN {
     private JButton borrarButton;
     private JButton salirButton;
     private Conexion BDD;
+    // Variable para guardar el nombre del campo con el foco
+    private String CampoConFoco = "";
 
     public LOGIN() {
         JButton botonBorrar = new JButton("Borrar");
+        // Asignar nombre a los campos de texto y contraseña
+        textField1.setName("usuario");
+        passwordField1.setName("contraseña");
+
+        // Obtener el campo de texto que tiene el foco
+        textField1.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // Acción cuando el campo de texto gana el foco (Capturar el nombre del campo con foco)
+                CampoConFoco=textField1.getName();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // Acción cuando el campo de texto pierde el foco
+            }
+        });
+        // Obtener el campo de contraseña que tiene el foco
+        passwordField1.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // Acción cuando el campo de texto gana el foco (Capturar el combre del campo con foco)
+                CampoConFoco=passwordField1.getName();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // Acción cuando el campo de texto pierde el foco
+            }
+        });
         borrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                borrarCampos();
+                String usuarioTexto=textField1.getText();
+                String contraseñaTexto=new String(passwordField1.getPassword());
+
+                // Verifica si hay caracteres en el campo de usuario antes de intentar borrar
+                // Y verificar si el ultimo campo con foco es el del usuario
+                if (CampoConFoco.equals("usuario")){
+                    // Comprobar si no esta vacio el campo de usuario
+                    if(!usuarioTexto.isEmpty()){
+                        // Borra la última letra del campo de usuario
+                        textField1.setText(usuarioTexto.substring(0, usuarioTexto.length()-1));
+                    }
+                }
+                // Verifica si hay caracteres en el campo de contraseña antes de intentar borrar
+                // Y verificar si el ultimo campo con foco es el de la contraseña
+                if (CampoConFoco.equals("contraseña")){
+                    // Comprobar si no esta vacio el campo de contraseña
+                    if(!contraseñaTexto.isEmpty()){
+                        // Borra la última letra del campo de usuario
+                        passwordField1.setText(contraseñaTexto.substring(0, contraseñaTexto.length()-1));
+                    }
+                }
             }
         });
         salirButton.addActionListener(new ActionListener() {
@@ -47,22 +101,5 @@ public class LOGIN {
                 }
             }
         });
-
-    }
-
-    private void borrarCampos(){
-        String usuarioTexto=textField1.getText();
-        String contraseñaTexto=new String(passwordField1.getPassword());
-        // Verifica si hay caracteres en el campo de usuario antes de intentar borrar
-        if(!usuarioTexto.isEmpty()){
-            // Borra la última letra del campo de usuario
-            textField1.setText(usuarioTexto.substring(0, usuarioTexto.length()-1));
-        }
-        if(!contraseñaTexto.isEmpty()){
-            passwordField1.setText(contraseñaTexto.substring(0, contraseñaTexto.length()-1));
-
-
-        }
-
     }
 }
