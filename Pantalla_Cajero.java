6d5b5d2c3c2705f9adf0267fe35caeab75954ace
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-21 11:27:02 -0500

    Cambio de Pantalla Silvia

--- Pantalla_Cajero.java
+++ Pantalla_Cajero.java
@@ -3,14 +3,18 @@ import javax.swing.*;
 public class Pantalla_Cajero {
     private JPanel PCajero;
     private JTabbedPane Panel1;
-    private JRadioButton elijaElProductoRadioButton;
     private JTable table1;
     private JTextField TXF1;
     private JTextField TXF2;
-    private JTextField TXF3;
     private JTextField textField4;
     private JButton Boton1;
     private JButton Boton2;
     private JButton Boton3;
     private JButton Boton4;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField1;
+    private JTextField textField5;
+    private JComboBox comboBox1;
+    private JSpinner spinner1;
 }
