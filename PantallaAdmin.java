5204eb463e12b4355828f5a583cf977fe9fdf480
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-20 22:03:19 -0500

    Validacion en el combobox de administrador

--- PantallaAdmin.java
+++ PantallaAdmin.java
@@ -23,27 +23,34 @@ public class PantallaAdmin {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String opcionSeleccionada = (String) comboboxOpciones.getSelectedItem();
+                System.out.println(opcionSeleccionada);
+                switch (opcionSeleccionada) {
 
-                switch (comboboxOpciones) {
-                    //
-                    case productosStock ignored:
+                    case "Productos en stock":
                         JFrame stock = new JFrame("Productos en Stock");
                         stock.setContentPane(new productosStock().productosStock);
                         stock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                         stock.setVisible(true);
                         stock.pack();
-                    case PantallaVendedores ignored:
+                        break;
+                    case "Vendores":
                         JFrame cajeros = new JFrame("Elija el cajero a su eleccion");
                         cajeros.setContentPane(new PantallaVendedores().Pantalla_Num_Cajero);
                         cajeros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                         cajeros.setVisible(true);
                         cajeros.pack();
-                    case PantallaAgregarC ignored:
+                        break;
+                    case "Agregar Cajero Nuevo":
                         JFrame agregarC = new JFrame("Cajero nuevo");
                         agregarC.setContentPane(new PantallaAgregarC().nuevo_cajero);
                         agregarC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                         agregarC.setVisible(true);
                         agregarC.pack();
+                        break;
+                    case null:
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opcion no valida");
                 }
 
             }
