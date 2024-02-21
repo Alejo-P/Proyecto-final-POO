b59ad5191350251cebc3271c8d5814fd4f8a71af
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-20 21:38:09 -0500

    Commit 20/02 (2)

--- PantallaAdmin.java
+++ PantallaAdmin.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PantallaAdmin {
-    private JComboBox comboBox1;
+    JComboBox<String> comboboxOpciones;
     private JButton salirButton;
     private JButton enterButton;
     public JPanel panel_admin;
@@ -22,23 +22,28 @@ public class PantallaAdmin {
         enterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame stock=new JFrame("Productos en Stock");
-                stock.setContentPane(new productosStock().productosStock);
-                stock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                stock.setVisible(true);
-                stock.pack();
+                String opcionSeleccionada = (String) comboboxOpciones.getSelectedItem();
 
-                JFrame cajeros=new JFrame("Elija el cajero a su eleccion");
-                cajeros.setContentPane(new PantallaVendedores().Pantalla_Num_Cajero);
-                cajeros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                cajeros.setVisible(true);
-                cajeros.pack();
-
-                JFrame agregarC=new JFrame("Cajero nuevo");
-                agregarC.setContentPane(new PantallaAgregarC().nuevo_cajero);
-                agregarC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                agregarC.setVisible(true);
-                agregarC.pack();
+                switch (comboboxOpciones) {
+                    case productosStock ignored:
+                        JFrame stock = new JFrame("Productos en Stock");
+                        stock.setContentPane(new productosStock().productosStock);
+                        stock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        stock.setVisible(true);
+                        stock.pack();
+                    case PantallaVendedores ignored:
+                        JFrame cajeros = new JFrame("Elija el cajero a su eleccion");
+                        cajeros.setContentPane(new PantallaVendedores().Pantalla_Num_Cajero);
+                        cajeros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        cajeros.setVisible(true);
+                        cajeros.pack();
+                    case PantallaAgregarC ignored:
+                        JFrame agregarC = new JFrame("Cajero nuevo");
+                        agregarC.setContentPane(new PantallaAgregarC().nuevo_cajero);
+                        agregarC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        agregarC.setVisible(true);
+                        agregarC.pack();
+                }
 
             }
         });
