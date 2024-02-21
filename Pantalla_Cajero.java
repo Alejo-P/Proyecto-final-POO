56be8f1ed6f395780e2abefd6c55c7c479a3fb00
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-21 12:02:15 -0500

    Cambios Silvia 3

--- Pantalla_Cajero.java
+++ Pantalla_Cajero.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class Pantalla_Cajero {
     private JPanel PCajero;
     private JTabbedPane Panel1;
-    private JTable table1;
     private JTextField TXF1;
     private JTextField TXF2;
     private JTextField textField4;
@@ -17,4 +16,17 @@ public class Pantalla_Cajero {
     private JTextField textField5;
     private JComboBox comboBox1;
     private JSpinner spinner1;
+    private JButton buscarButton;
+    private JTextField textField7;
+    private JTabbedPane tabbedPane1;
+    private JButton volverButton;
+    private JButton imprimirButton;
+    private JButton seleccionarButton;
+    private JTable table2;
+    private JTable table1;
+    private JTextField textField8;
+    private JButton buscarButton1;
+    private JTable table3;
+    private JButton actualizarButton;
+    private JButton eliminarButton;
 }
