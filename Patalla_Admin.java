03a9fc49faba0044c2d1b92f2ec1fe73b6640044
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-02-22 00:35:48 -0500

    Commit 22/02 (1)

---
+++ Patalla_Admin.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Patalla_Admin {
+    private JTabbedPane tabbedPane1;
+    private JComboBox comboBox1;
+    private JButton comprarButton;
+    private JComboBox comboBox2;
+    private JButton accederButton;
+    private JButton salirButton;
+    private JButton salirButton1;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JPasswordField passwordField2;
+    private JButton agregarButton;
+}
